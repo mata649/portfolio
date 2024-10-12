d7c36bdd2a4a1c6eabfa8f6991416ac8ba28de9a
Jose Mata <mata649@hotmail.com>
2024-10-12 00:52:10 -0600

    admin tests added

---
+++ test/portfolio_web/live/admin_test.exs
@@ -0,0 +1,20 @@
+defmodule PortfolioWeb.AdminTest do
+  use PortfolioWeb.ConnCase, async: true
+  import Phoenix.LiveViewTest
+  import Portfolio.AccountsFixtures
+
+  describe "Admin page" do
+    test "renders admin page", %{conn: conn} do
+      {:ok, _lv, html} = conn |> log_in_user(user_fixture()) |> live(~p"/admin")
+      assert html =~ "Jose Mata"
+    end
+
+    test "redirects if user not logged in", %{conn: conn} do
+      {:error, redirect} = live(conn, ~p"/admin")
+      assert {:redirect, %{to: path, flash: flash}} = redirect
+      assert path == ~p"/"
+      assert %{"error" => message} = flash
+      assert message == "Nope!"
+    end
+  end
+end
