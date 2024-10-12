c1115f05eb79f402820b397c297576fd4de4ea04
Jose Mata <mata649@hotmail.com>
2024-10-11 22:21:31 -0600

    extra authentication stuff removed
    I will be the only one managing the site. User creation, password
    resets, etc. will be done via a CLI tool.

--- lib/portfolio_web/controllers/user_session_controller.ex
+++ lib/portfolio_web/controllers/user_session_controller.ex
@@ -4,16 +4,6 @@ defmodule PortfolioWeb.UserSessionController do
   alias Portfolio.Accounts
   alias PortfolioWeb.UserAuth
 
-  def create(conn, %{"_action" => "registered"} = params) do
-    create(conn, params, "Account created successfully!")
-  end
-
-  def create(conn, %{"_action" => "password_updated"} = params) do
-    conn
-    |> put_session(:user_return_to, ~p"/users/settings")
-    |> create(params, "Password updated successfully!")
-  end
-
   def create(conn, params) do
     create(conn, params, "Welcome back!")
   end
