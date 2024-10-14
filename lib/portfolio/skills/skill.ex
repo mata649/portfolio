513ce919e27e3afaeae3eb31807781c4b641594b
Jose Mata <mata649@hotmail.com>
2024-10-13 20:04:40 -0600

    valid name length increased

--- lib/portfolio/skills/skill.ex
+++ lib/portfolio/skills/skill.ex
@@ -14,7 +14,7 @@ defmodule Portfolio.Skills.Skill do
     skill
     |> cast(attrs, [:name, :color])
     |> validate_required([:name, :color])
-    |> validate_length(:name, max: 15)
+    |> validate_length(:name, max: 30)
     |> validate_length(:color, is: 7)
     |> validate_format(:color, ~r/^#(?:[0-9a-fA-F]{3}){1,2}$/)
   end
