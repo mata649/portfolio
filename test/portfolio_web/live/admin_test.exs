380dba52674dd64586cf75241d8a51e48db4adc9
Jose Mata <mata649@hotmail.com>
2024-10-13 21:26:44 -0600

    click Skills button test added

--- test/portfolio_web/live/admin_test.exs
+++ test/portfolio_web/live/admin_test.exs
@@ -16,5 +16,15 @@ defmodule PortfolioWeb.AdminTest do
       assert %{"error" => message} = flash
       assert message == "Nope!"
     end
+
+    test "click on Skills redirects to skills index", %{conn: conn} do
+      conn = log_in_user(conn, user_fixture())
+      {:ok, lv, _html} = conn |> live(~p"/admin")
+
+      {:ok, _lv, skills_index_html} =
+        lv |> element("a", "Skills") |> render_click() |> follow_redirect(conn)
+
+      skills_index_html =~ "Listing Skills"
+    end
   end
 end
