ed7a81485096fb00c1e6dc0404ca4cb8997db902
Jose Mata <mata649@hotmail.com>
2024-10-13 20:44:56 -0600

    auto-generated projects module added

---
+++ lib/portfolio/projects/project.ex
@@ -0,0 +1,19 @@
+defmodule Portfolio.Projects.Project do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "projects" do
+    field :name, :string
+    field :description, :string
+    field :githubURL, :string
+
+    timestamps(type: :utc_datetime)
+  end
+
+  @doc false
+  def changeset(project, attrs) do
+    project
+    |> cast(attrs, [:name, :description, :githubURL])
+    |> validate_required([:name, :description, :githubURL])
+  end
+end
---
+++ test/support/fixtures/projects_fixtures.ex
@@ -0,0 +1,22 @@
+defmodule Portfolio.ProjectsFixtures do
+  @moduledoc """
+  This module defines test helpers for creating
+  entities via the `Portfolio.Projects` context.
+  """
+
+  @doc """
+  Generate a project.
+  """
+  def project_fixture(attrs \\ %{}) do
+    {:ok, project} =
+      attrs
+      |> Enum.into(%{
+        description: "some description",
+        githubURL: "some githubURL",
+        name: "some name"
+      })
+      |> Portfolio.Projects.create_project()
+
+    project
+  end
+end
