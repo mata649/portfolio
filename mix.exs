7ffda0e8632857ae1ac8b33fa0abc80cf3f623e7
Jose Mata <mata649@hotmail.com>
2024-10-20 19:54:40 -0600

    live_select and credo added

--- mix.exs
+++ mix.exs
@@ -59,7 +59,9 @@ defmodule Portfolio.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:live_select, "~> 1.4"},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
     ]
   end
 
