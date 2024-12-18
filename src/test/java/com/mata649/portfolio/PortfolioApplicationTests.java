09c6a5b64d41880329603081013032634e5fc75f
Jose Mata <mata649@hotmail.com>
2024-12-18 14:11:40 -0600

    init project

---
+++ src/test/java/com/mata649/portfolio/PortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mata649.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
