f9ec83d76427ed8215a11fa928f9c2ed6d05bd93
Jose Mata <mata649@hotmail.com>
2025-01-18 14:00:26 -0600

    change folder name from shared to components

--- src/main/java/com/mata649/portfolio/frontend/views/components/ConfirmationDialog.java
+++ src/main/java/com/mata649/portfolio/frontend/views/components/ConfirmationDialog.java
@@ -1,4 +1,4 @@
-package com.mata649.portfolio.frontend.views.shared;
+package com.mata649.portfolio.frontend.views.components;
 
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dialog.Dialog;
