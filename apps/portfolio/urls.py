a42d5b9198468c1df38c97851808d664680625c8
Jose Mata <mata649@hotmail.com>
2025-09-05 13:23:28 -0600

    refactor filters from fbv to clv

--- apps/portfolio/urls.py
+++ apps/portfolio/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.HomePageView.as_view(), name='home'),
-    path('filter_projects/', views.filter_projects, name='filter_projects'),
+    path('filter_projects/', views.ProjectFilterView.as_view(), name='filter_projects'),
+    path('filter_experiences/', views.ExperienceFilterView.as_view(), name='filter_experiences'),
 ]
--- apps/portfolio/views.py
+++ apps/portfolio/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
 from django.db.models import Q
 
 from apps.portfolio.models import Skill, SiteInfo, Experience, Project
@@ -13,19 +13,38 @@ class HomePageView(TemplateView):
         context = super(HomePageView, self).get_context_data(**kwargs)
         context['skills'] = Skill.objects.all()
         context['site_info'] = SiteInfo.objects.first()
-        context['projects'] = Project.objects.prefetch_related('skills').all()
-        context['experiences'] = Experience.objects.all()
+        context['projects'] = Project.objects.prefetch_related('skills').all().order_by('year')
+        context['experiences'] = Experience.objects.prefetch_related('skills').all().order_by('-start_date')
         return context
 
 
-def filter_projects(request):
-    skills_param = request.GET.get('skills')
-    projects = Project.objects.all()
-    if skills_param:
-        skill_list = skills_param.split(',')
-        for skill in skill_list:
-            projects = Project.objects.filter(skills__name__iexact=skill)
+class ProjectFilterView(ListView):
+    model = Project
+    template_name = 'portfolio/partials/project_list.html'
+    context_object_name = 'projects'
 
-    return render(request, 'portfolio/partials/project_list.html', {
-        'projects': projects
-    })
+    def get_queryset(self):
+        projects = Project.objects.all()
+        skills_param = self.request.GET.get('skills')
+        if skills_param:
+            skill_list = skills_param.split(',')
+            for skill in skill_list:
+                projects = projects.filter(skills__name__iexact=skill)
+        return projects.order_by('year')
+
+
+class ExperienceFilterView(ListView):
+    model = Experience
+    template_name = 'portfolio/partials/experience_list.html'
+    context_object_name = 'experiences'
+
+    def get_queryset(self):
+        experiences = Experience.objects.all()
+        skills_param = self.request.GET.get('skills')
+
+        if skills_param:
+            skill_list = skills_param.split(',')
+            for skill in skill_list:
+                experiences = experiences.filter(skills__name__iexact=skill)
+
+        return experiences.order_by('-start_date')
