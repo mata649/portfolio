5b6f9a51a7521618ad575e21bda0a00d0016be75
Jose Mata <mata649@hotmail.com>
2025-09-07 09:58:03 -0600

    RDS free tier is done

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -14,6 +14,6 @@ ALLOWED_HOSTS = []
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db_dev.sqlite3',
     }
 }
--- config/settings/prod.py
+++ config/settings/prod.py
@@ -13,11 +13,7 @@ ALLOWED_HOSTS = []
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config.DB_NAME,
-        'USER': config.DB_USER,
-        'PASSWORD': config.DB_PASSWORD,
-        'HOST': config.DB_HOST,
-        'PORT': config.DB_PORT,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db_prod.sqlite3',
     }
 }
