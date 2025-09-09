b59aa840d5de840763bead033d293042d36e3818
Jose Mata <mata649@hotmail.com>
2025-09-08 19:35:00 -0600

    add required production config

--- config/settings/base.py
+++ config/settings/base.py
@@ -78,6 +78,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles_collected'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
--- config/settings/prod.py
+++ config/settings/prod.py
@@ -6,7 +6,7 @@ SECRET_KEY = config.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
