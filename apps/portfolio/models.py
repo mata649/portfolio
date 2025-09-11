80af153d1eba08bcf5154671f3d3f603f1a9ccf7
Jose Mata <mata649@hotmail.com>
2025-09-11 13:37:06 -0600

    add display_first filter to Skill model

--- apps/portfolio/models.py
+++ apps/portfolio/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 class Skill(models.Model):
     name = models.CharField(blank=False, max_length=30)
-
+    display_first = models.BooleanField(default=False)
     def __str__(self):
         return self.name
 
